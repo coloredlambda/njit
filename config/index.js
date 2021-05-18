cc207ad5442891706607adf3b0405d6469cc67bf
Raaj <raaj.ahab@gmail.com>
2021-05-18 18:06:32 +0000

    adds login endpoint

--- config/index.js
+++ config/index.js
@@ -1,8 +1,13 @@
 import dotenv from 'dotenv'
-import { defaultTo, equals } from '@meltwater/phi'
+import { defaultTo, equals, isNilOrEmpty, when } from '@meltwater/phi'
 import { name as applicationName } from '../package.json'
 
 dotenv.config({})
+
+export const throwWhenNoJWT = when(
+  isNilOrEmpty,
+  () => { throw new Error('JWT secret not set. Set in env var - JWT_SECRET_KEY') }
+)
 
 export default ({
   applicationName,
@@ -12,6 +17,12 @@ export default ({
       uri: defaultTo('mongodb://localhost:27017/njit', process.env.MONGODB_DATABASE_URI),
       useUnifiedTopology: equals('true', process.env.MONGODB_USE_UNIFIED_TOPOLOGY),
       useNewUrlParser: equals('true', process.env.MONGODB_USE_NEW_URL_PARSER)
+    }
+  },
+  authentication: {
+    jwt: {
+      secretKey: throwWhenNoJWT(process.env.JWT_SECRET_KEY),
+      expirationTime: defaultTo('2 days', process.env.JWT_EXPIRATION_TIME)
     }
   }
 })
