9c34f07e7c60ce122239ea5e4d2365a156ebae08
Raaj <raaj.ahab@gmail.com>
2021-05-19 07:30:17 +0000

    adds password management endpoints with accompanying logic

--- config/index.js
+++ config/index.js
@@ -4,11 +4,6 @@ import { name as applicationName } from '../package.json'
 
 dotenv.config({})
 
-export const throwWhenNoJWT = when(
-  isNilOrEmpty,
-  () => { throw new Error('JWT secret not set. Set in env var - JWT_SECRET_KEY') }
-)
-
 export default ({
   applicationName,
   port: defaultTo(5000, process.env.PORT),
@@ -21,8 +16,14 @@ export default ({
   },
   authentication: {
     jwt: {
-      secretKey: throwWhenNoJWT(process.env.JWT_SECRET_KEY),
+      secretKey: defaultTo('90jw98hnw9uehnfiuhgwu9hg9u', process.env.JWT_SECRET_KEY),
       expirationTime: defaultTo('2 days', process.env.JWT_EXPIRATION_TIME)
     }
+  },
+  email: {
+    host: defaultTo('smtp.gmail.com', process.env.EMAIL_HOST),
+    port: defaultTo('587', process.env.EMAIL_PORT),
+    username: defaultTo('raaj.burner@gmail.com', process.env.EMAIL_USERNAME),
+    password: defaultTo('sobdos-rymJec-sutbi2', process.env.EMAIL_PASSWORD)
   }
 })
